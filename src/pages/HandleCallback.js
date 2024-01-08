270fedc450afc775c8dd2f03b22bccbad33745c4
rohseh303 <rohansehgal935@gmail.com>
2024-01-07 21:31:09 -0800

    set authentication state variable to global one

--- src/pages/HandleCallback.js
+++ src/pages/HandleCallback.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import SignIn from '../components/SignIn';
+import GlobalVariableProvider from './MainStorePage'
+import { components } from '@aws-amplify/ui-react';
+import MainStorePage from './MainStorePage';
 
 const HandleCallback = () => {
     // do work in database to show that they've signed in succesfully
+    const { isAuthenticated, setIsAuthenticated } = React.useContext(GlobalVariableProvider);
 
   return (
-    <SignIn/>
+    <SignIn setIsAuthenticated={setIsAuthenticated} />
   )
 }
 
--- src/pages/MainStorePage.js
+++ src/pages/MainStorePage.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
 import SignIn from '../components/SignIn';
 
-function MainStorePage() {
+import { createContext, useContext } from 'react';
+const GlobalVariableContext = createContext();
+
+const GlobalVariableProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return (
+      <GlobalVariableContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+          {children}
+      </GlobalVariableContext.Provider>
+  );
+}
+
+function MainStorePage() {
+  const { isAuthenticated, setIsAuthenticated } = React.useContext(GlobalVariableProvider);
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
     <div>
