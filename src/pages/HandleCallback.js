39c8a4a71433b04799ce003c0ce6ec60db8bcfd0
rohseh303 <rohansehgal935@gmail.com>
2024-01-07 23:08:11 -0800

    updating to add log

--- src/pages/HandleCallback.js
+++ src/pages/HandleCallback.js
@@ -2,6 +2,8 @@ import SignIn from '../components/SignIn';
 
 const HandleCallback = () => {
 
+  console.log("data work will go here! set square auth status to true, update db etc.")
+
   return (
     <SignIn />
   );
